c9ce9f8c2261a699be597b4e9c6ee34e8c514a28
zackattacknyu <zackattacknyu@gmail.com>
2015-11-14 18:00:15 -0800

    sampling done well

--- samplingScript2D.m
+++ samplingScript2D.m
@@ -1,4 +1,4 @@
-fileNum=15;
+fileNum=25;
 
 dataFiles = dir('data/compiledData/data*');
 load(['data/compiledData/' dataFiles(fileNum).name]);
@@ -9,121 +9,14 @@ ir = ir(1:625,:);
 obs = obs(1:625,:); 
 
 curImage = rr1;
-curImage(curImage<0)=0;
-
-%make octree for all the slots where sample
-%   could come from in (x,y,t) space
 minDist = 15;
 patchSize = 20;
-slotDist=minDist/2;
-slots = zeros(floor(size(curImage)/slotDist)+1);
-
-
-maxTotalPatches = numel(slots);
-patchSum = zeros(1,maxTotalPatches);
-randPatches = cell(1,maxTotalPatches);
-randPatchesCornerCoord = cell(1,maxTotalPatches);
-
-%gets indices using precip map as PDF
-imageValues = curImage(:);
-cdfX = cumsum(imageValues./sum(imageValues));
-
-imgIndex=1;
 maxTries = 1000;
+maxNumPatches = 50;
 
-for k = 1:maxTries
+[ randPatches, randPatchesCornerCoord, patchSum ] = ...
+    getSampledPatches( curImage, patchSize, minDist, maxNumPatches, maxTries );
 
-    curSample = find(rand<cdfX, 1 );
-   [randStartRow,randStartCol] = ind2sub(size(curImage),curSample);
-   if(randStartRow-patchSize/2 < 1 || randStartRow+patchSize/2 > size(curImage,1))
-      continue; 
-   end
-   if(randStartCol-patchSize/2 < 1 || randStartCol+patchSize/2 > size(curImage,2))
-      continue; 
-   end
-   
-   %if(imgIndex>200)
-   %   break; 
-   %end
-   
-   slotX = floor(randStartRow/slotDist)+1;
-   slotY = floor(randStartCol/slotDist)+1;
-
-   if(slots(slotX,slotY) > 0)
-      continue; 
-   end
-   
-   curLocation = [randStartRow randStartCol];
-   %tests if others are near
-    isBad=false;
-    for j=1:(imgIndex-1)
-        iLoc = randPatchesCornerCoord{j};
-        if(norm(curLocation-iLoc)<minDist)
-           isBad=true;
-           break;
-        end
-    end
-    if(isBad)
-       continue; 
-    end
-    
-   
-
-   slots(slotX,slotY)=1;
-
-   randPatch = curImage(...
-       (randStartRow-patchSize/2):(randStartRow+patchSize/2-1),...
-       (randStartCol-patchSize/2):(randStartCol+patchSize/2-1));
-
-   
-
-   ourPatch = randPatch;
-   curPatchSum = sum(ourPatch(:));
-
-   if(curPatchSum > 0)
-        patchSum(imgIndex) = curPatchSum;
-        randPatches{imgIndex} = ourPatch;
-        randPatchesCornerCoord{imgIndex} = curLocation;
-        imgIndex = imgIndex+1;
-
-        if(mod(imgIndex,100) == 0)
-           imgIndex 
-        end
-   end
-
-
-end
-
-patchSum = patchSum(1:(imgIndex-1));
-randPatches = randPatches(1:(imgIndex-1));
-randPatchesCornerCoord = randPatchesCornerCoord(1:(imgIndex-1));
-
-maxNum = length(randPatches);
-patchesCornerCoord = cell(1,maxNum);
-curIndex=1;
-
-%{
-%closest ones
-for i = 1:maxNum
-    patchLoc = randPatchesCornerCoord{i};
-    numBad=0;
-    for j=1:(curIndex-1)
-        curLoc = patchesCornerCoord{j};
-        if(norm(patchLoc-curLoc)<minDist)
-           numBad = numBad+1; 
-        end
-    end
-    if(numBad<1)
-        patchesCornerCoord{curIndex} = patchLoc;
-        curIndex = curIndex + 1;
-    end
-    if(mod(i,100)==0)
-       i 
-    end
-end
-patchesCornerCoord = patchesCornerCoord(1:(curIndex-1));
-%}
-patchesCornerCoord = randPatchesCornerCoord;
 
 figure(1)
 imagesc(curImage)
@@ -132,8 +25,8 @@ caxis([-1 20])
 drwvect([-135 25 -65 50],[625 1750],'us_states_outl_ug.tmp','k');
 colorbar('vertical')
 hold on
-for i = 1:length(patchesCornerCoord)
-   centerLoc = patchesCornerCoord{i};
+for i = 1:length(randPatchesCornerCoord)
+   centerLoc = randPatchesCornerCoord{i};
    centerLoc = centerLoc - [patchSize/2 patchSize/2];
    rectangle('Position',[centerLoc(2) centerLoc(1) patchSize patchSize]);
 end
