4f08ba3bf2dd0237da8e77f81ec7b03aae0511e9
zackattacknyu <zackattacknyu@gmail.com>
2015-11-15 18:53:30 -0800

    progress on display of patches

--- patchErrorScript.m
+++ patchErrorScript.m
@@ -19,8 +19,27 @@ maxNumPatches = 100;
 
 oldPredPatches = getPatchesFromCoords(rr1,randPatchesCornerCoord,patchSize);
 newPredPatches = getPatchesFromCoords(rr2,randPatchesCornerCoord,patchSize);
-
-
+%%
+targetPatches2 = cell(1,length(targetPatches));
+oldPredPatches2 = cell(1,length(oldPredPatches));
+newPredPatches2 = cell(1,length(newPredPatches));
+newInd = 1;
+for i = 1:length(targetPatches)
+    curTarget = targetPatches{i};
+    curOldPred = oldPredPatches{i};
+    curNewPred = newPredPatches{i};
+    if(sum(curTarget(:))>0 && sum(curOldPred(:))>0 && sum(curNewPred(:))>0)
+       targetPatches2{newInd} = targetPatches{i};
+       oldPredPatches2{newInd} = oldPredPatches{i};
+       newPredPatches2{newInd} = newPredPatches{i};
+       newInd = newInd + 1;
+    end
+end
+targetPatches = targetPatches2(1:(newInd-1));
+oldPredPatches = oldPredPatches2(1:(newInd-1));
+newPredPatches = newPredPatches2(1:(newInd-1));
+%%
+%{
 figure
 drawMapWithPatches(curImage,randPatchesCornerCoord,patchSize);
 
@@ -29,7 +48,7 @@ subplot(1,2,1);
 drawMapWithPatches(rr1,randPatchesCornerCoord,patchSize);
 subplot(1,2,2);
 drawMapWithPatches(rr2,randPatchesCornerCoord,patchSize);
-
+%}
 
 oldPredErrors = zeros(1,length(targetPatches));
 newPredErrors = zeros(1,length(targetPatches));
@@ -39,13 +58,13 @@ for i = 1:length(targetPatches)
     curTarget = targetPatches{i};
     curOldPred = oldPredPatches{i};
     curNewPred = newPredPatches{i};
-   oldPredErrors(i) = rmsePatches(curTarget,curOldPred);
-   newPredErrors(i) = rmsePatches(curTarget,curNewPred);
+    oldPredErrors(i) = rmsePatches(curTarget,curOldPred);
+    newPredErrors(i) = rmsePatches(curTarget,curNewPred);
    
-   i
+    i
    
-   %oldPredErrorsEMD(i) = getEMDwQP(curTarget,curOldPred);
-   %newPredErrorsEMD(i) = getEMDwQP(curTarget,curNewPred);
+    %oldPredErrorsEMD(i) = getEMDwQP(curTarget,curOldPred);
+    %newPredErrorsEMD(i) = getEMDwQP(curTarget,curNewPred);
 end
 
 meanOldError = mean(oldPredErrors)
@@ -53,6 +72,20 @@ meanNewError = mean(newPredErrors)
 
 %meanOldErrorEMD = mean(oldPredErrorsEMD)
 %meanNewErrorEMD = mean(newPredErrorsEMD)
+
+indices = randperm(length(targetPatches));
+numDisplay = 5;
+figure
+for i = 1:numDisplay
+    patchNum = indices(i);
+    pInd = 3*(i-1);
+    subplot(numDisplay,3,1+pInd);
+    imagesc(targetPatches{i});
+    subplot(numDisplay,3,2+pInd);
+    imagesc(oldPredPatches{i});
+    subplot(numDisplay,3,3+pInd);
+    imagesc(newPredPatches{i});
+end
 
 
 
