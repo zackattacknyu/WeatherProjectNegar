fb458aa3b7891f62c16fb61bd10deaabadf67382
zackattacknyu <zackattacknyu@gmail.com>
2015-12-22 23:34:45 -0800

    got randomization working well

--- predictionUI/init.m
+++ predictionUI/init.m
@@ -14,10 +14,21 @@ selections = zeros(1,size(dispPatches,2));
 %%
 remainingPatches = find(selections==0); %-1 values are changed to zero so we don't need to care here
 
+remainingPatches = remainingPatches(randperm(length(remainingPatches)));
 for i = 1:length(remainingPatches)
     j = remainingPatches(i);
     patchSet = dispPatches(:,j);
-    aa = bestPredUI(patchSet)
+    patchSet2 = {patchSet{1},patchSet{3},patchSet{2}};
+    if(rand>0.5) %flip order of them
+        aa = bestPredUI(patchSet2);
+        if(aa==1)
+            aa=2;
+        elseif(aa==2) 
+            aa=1;
+        end
+    else
+        aa = bestPredUI(patchSet);
+    end
     selections(j) = aa;
     if(aa == 0)
         break;
