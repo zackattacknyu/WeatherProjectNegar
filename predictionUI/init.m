06ae6e826382febd4808ce6f51205a9eacf78670
zackattacknyu <zackattacknyu@gmail.com>
2015-12-25 00:13:39 -0800

    work done

--- predictionUI/init.m
+++ predictionUI/init.m
@@ -86,7 +86,7 @@ ylabel('Fraction of Choices');
 resultEntries = find(selections~=0);
 resultVals = selections(resultEntries);
 
-binSize=15;
+binSize=20;
 numBins = floor(length(resultVals)/binSize);
 numInLastBin = length(resultVals)-numBins*binSize;
 resultValsBinned = mat2cell(resultVals,1,[binSize*ones(1,numBins) numInLastBin]);
@@ -96,9 +96,9 @@ probMSE = zeros(1,length(resultValsBinned));
 probAmb = zeros(1,length(resultValsBinned));
 for i = 1:length(resultValsBinned)
    curBin = resultValsBinned{i};
-   probEMD(i) = length(find(curBin==1))/binSize;
-   probMSE(i) = length(find(curBin==2))/binSize;
-   probAmb(i) = length(find(curBin==-2))/binSize;
+   probEMD(i) = length(find(curBin==1))/length(curBin);
+   probMSE(i) = length(find(curBin==2))/length(curBin);
+   probAmb(i) = length(find(curBin==-2))/length(curBin);
 end
 
 figure
@@ -106,7 +106,8 @@ hold on
 plot(probEMD,'g-');
 plot(probMSE,'r-');
 plot(probAmb,'b-');
-legend('EMD Probablity','MSE Probability','Ambiguous Probability');
+legend('EMD Probablity','MSE Probability','Ambiguous Probability',...
+    'location','eastoutside');
 hold off
 
 
