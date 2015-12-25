c2e513a0572e63980e998608664403bb6d031ff1
zackattacknyu <zackattacknyu@gmail.com>
2015-12-24 23:11:56 -0800

    work

--- predictionUI/init.m
+++ predictionUI/init.m
@@ -19,34 +19,46 @@ loadFileName = strcat('patchesSet11-23Data_',numString,'.mat');
 loadResultsFileName = strcat('patchesSet11-23Data_',numString,'_results.mat');
 
 getDispPatchesScript
-
-selections = zeros(1,size(dispPatches,2));
 %%
-remainingPatches = find(selections==0); %-1 values are changed to zero so we don't need to care here
+selections = zeros(1,size(dispPatches,2));
 
-remainingPatches = remainingPatches(randperm(length(remainingPatches)));
-for i = 1:length(remainingPatches)
-    j = remainingPatches(i);
-    patchSet = dispPatches(:,j);
-    if(rand>0.5) %flip order of them
-        patchSet2 = {patchSet{1},patchSet{3},patchSet{2}};
-        aa = bestPredUI(patchSet2);
-        if(aa==1)
-            aa=2;
-        elseif(aa==2) 
-            aa=1;
+
+remainingPatches = 1:size(dispPatches,2);
+
+while(~isempty(remainingPatches))
+
+    remainingPatches = remainingPatches(randperm(length(remainingPatches)));
+    for i = 1:length(remainingPatches)
+        j = remainingPatches(i);
+        patchSet = dispPatches(:,j);
+        if(rand>0.5) %flip order of them
+            patchSet2 = {patchSet{1},patchSet{3},patchSet{2}};
+            aa = bestPredUI(patchSet2);
+            if(aa==1)
+                aa=2;
+            elseif(aa==2) 
+                aa=1;
+            end
+        else
+            aa = bestPredUI(patchSet);
         end
-    else
-        aa = bestPredUI(patchSet);
+        selections(j) = aa;
+        if(aa == 0)
+            break;
+        end
+        %uiwait
     end
-    selections(j) = aa;
-    if(aa == 0)
-        break;
+    
+    selections(selections==-1)=0;
+    remainingPatches = find(selections==0); %-1 values are changed to zero so we don't need to care here
+    
+    if(aa==0)
+       break; 
     end
-    %uiwait
+    
 end
 close all force
-selections(selections==-1)=0;
+
 %%
 numEMD = length(find(selections==1));
 numMSE = length(find(selections==2));
