6da7691b4165b9a8f29d9d954ec949d4b93fe324
zackattacknyu <zackattacknyu@gmail.com>
2016-01-05 15:01:45 -0800

    work

--- predictionUI/analyzeRanges.m
+++ predictionUI/analyzeRanges.m
@@ -1,12 +1,13 @@
 mseSorted = sort([mseOthers mseBests]);
 emdSorted = sort([emdOthers emdBests]);
 
+%{
 emdSorted = emdSorted(1:end-20);
 mseSorted = mseSorted(1:end-20);
 
 mseSorted = mseSorted./max(mseSorted);
 emdSorted = emdSorted./max(emdSorted);
-
+%}
 figure
 hold on
 plot(mseSorted,'r-');
