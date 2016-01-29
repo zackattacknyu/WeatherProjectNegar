17bd22636b7eb6678cd3ba78df149699ce8c822f
zackattacknyu <zackattacknyu@gmail.com>
2016-01-28 19:36:06 -0800

    done

--- predictionUI_release_postProcessing/getMeasure.m
+++ predictionUI_release_postProcessing/getMeasure.m
@@ -9,8 +9,27 @@ numTrueNeg = sum((predBinary==0)&(trainBinary==0));
 numFalsePos = sum((predBinary==1)&(trainBinary==0));
 numFalseNeg = sum((predBinary==0)&(trainBinary==1));
 
-prob = (numTruePos+numTrueNeg)/numPixels;
+%probability of detection
+%prob = (numTruePos+numTrueNeg)/numPixels;
+
+%false alarm rate
 %prob = (numFalsePos)/(numFalsePos+numTrueNeg);
+
+%bias score
+%{
+if(numTruePos+numFalseNeg<1)
+    prob=0;
+else
+    prob = (numTruePos+numFalsePos)/(numTruePos+numFalseNeg);
+end
+%}
+
+%success ratio
+if(numTruePos+numFalsePos<1)
+    prob=1;
+else
+    prob = numTruePos/(numTruePos+numFalsePos);
+end
 
 end
 
