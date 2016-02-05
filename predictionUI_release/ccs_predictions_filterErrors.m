ddf3e17ee23f7f04e50bbb707f4c66ddb2047586
zackattacknyu <zackattacknyu@gmail.com>
2016-02-05 04:30:59 -0800

    work

--- predictionUI_release/ccs_predictions_filterErrors.m
+++ predictionUI_release/ccs_predictions_filterErrors.m
@@ -1,11 +1,11 @@
 interestingThresh = 5; %max amount of precipitation required in patch to be considered
-maxPixelValues = zeros(5,length(targetPatches));
+maxPixelValues = zeros(size(predPatches,1)+1,length(targetPatches));
 for i = 1:length(targetPatches)
     
     curPatch = targetPatches{i};
     maxPixelValues(1,i) = max(curPatch(:));
     
-    for j = 1:4
+    for j = 1:size(predPatches,1)
         otherPatch = predPatches{j,i};
         maxPixelValues(j+1,i) = max(otherPatch(:));
     end
