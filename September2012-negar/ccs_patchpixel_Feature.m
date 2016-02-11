6b65420a1d1512b4236b87f10882678135f9f05e
zackattacknyu <zackattacknyu@gmail.com>
2016-02-11 03:29:54 -0800

    improved patch pixel code

--- September2012-negar/ccs_patchpixel_Feature.m
+++ September2012-negar/ccs_patchpixel_Feature.m
@@ -1,6 +1,7 @@
 function F = ccs_patchpixel_Feature(IR,l,maxl,ff)
 
-FDATA = []
+FDATAcell = cell(1,maxl);
+numRows = 0;
 
 for j=1:maxl
     
@@ -9,18 +10,29 @@ for j=1:maxl
       temp = IR(kk);
       feat = ff(j,:);
       
+      DATA = zeros(length(temp),13);
       DATA(:,1) = temp;
       DATA(:,2:13) = repmat(feat,length(kk),1);
             
-      FDATA = [FDATA;DATA];
+      FDATAcell{j}=DATA;
       
-      clear DATA
-      clear temp
-      clear rain
+      numRows = numRows + size(DATA,1);
+      
+      %clear DATA
+      %clear temp
+end
+
+FDATA = zeros(numRows,13);
+
+curIndex=1;
+for j =1:maxl
+    DATA = FDATAcell{j};
+    numRowData = size(DATA,1);
+    FDATA(curIndex:(curIndex+numRowData-1),:)=DATA;
+    curIndex=curIndex+numRowData;
 end
   
- nn = find(FDATA(:,13) >= 0);
-%  
+ nn = (FDATA(:,13) >= 0);
  FDATA = FDATA(nn,:);
 
 F = FDATA;
