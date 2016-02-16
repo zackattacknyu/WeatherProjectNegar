034f3f78ea88143aee8634d26d3408e4bc400980
zackattacknyu <zackattacknyu@gmail.com>
2016-02-15 16:34:25 -0800

    sd

--- decisionTreeNick/run3.m
+++ decisionTreeNick/run3.m
@@ -7,9 +7,61 @@ boostArgs.nIter = 1000;
 boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
 boostArgs.v = 0.1;     
 boostArgs.funargs = {rf J};
+%%
 [perfTrain,perfTest,boostStruct] = ...
     BoostLS(XTrPct, Ytr, XTePct, Yte, @boostTreeFun, boostArgs, [], []);
 
 %%
 
 Yte2 = boostTreeVal2(boostStruct,1000,XTePct,0.1);
+
+%%
+
+%Jvals = [2 4 8 16 32 64];
+Jvals = [128 256 512 1024 2048];
+testRMSE = zeros(1,length(Jvals));
+rf = 0.1;
+boostArgs.nIter = 400;  
+boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
+boostArgs.v = 0.1;     
+for jj = 1:length(Jvals)
+    J = Jvals(jj);
+    J
+    boostArgs.funargs = {rf J};
+    [perfTrain,perfTest,boostStruct] = ...
+        BoostLS(XTrPct, Ytr, XTePct, Yte, @boostTreeFun, boostArgs, [], []);
+    testRMSE(jj) = boostStruct.perfTest(end).rmse;
+end
+%%
+J = 64;
+boostArgs.v = 0.1;
+rfVals = [0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1];
+testRMSErf = zeros(1,length(rfVals));
+boostArgs.nIter = 300;  
+boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
+for jj = 1:length(rfVals)
+    rf = rfVals(jj);
+    rf
+    boostArgs.funargs = {rf J};
+    [perfTrain,perfTest,boostStruct] = ...
+        BoostLS(XTrPct, Ytr, XTePct, Yte, @boostTreeFun, boostArgs, [], []);
+    testRMSErf(jj) = boostStruct.perfTest(end).rmse;
+end
+
+J = 64;
+vVals = [0.1 0.2 0.25 0.4 0.5 0.7 0.9 0.8 1.0 1.25 1.5 1.75 2.0];
+testRMSEv = zeros(1,length(vVals));
+rf = 0.1;
+boostArgs.nIter = 300;  
+boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
+boostArgs.funargs = {rf J};
+for jj = 1:length(vVals)
+    boostArgs.v = vVals(jj);     
+    boostArgs.v
+    [perfTrain,perfTest,boostStruct] = ...
+        BoostLS(XTrPct, Ytr, XTePct, Yte, @boostTreeFun, boostArgs, [], []);
+    testRMSEv(jj) = boostStruct.perfTest(end).rmse;
+end
+
+
+
