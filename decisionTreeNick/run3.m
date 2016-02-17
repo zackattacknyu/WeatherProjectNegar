55ef3c95d9a74d98c85cbb88b31fd42ec9e46fbf
zackattacknyu <zackattacknyu@gmail.com>
2016-02-17 01:55:28 -0800

    current run

--- decisionTreeNick/run3.m
+++ decisionTreeNick/run3.m
@@ -1,7 +1,7 @@
 feaToUse = 1:size(Xtr,2);
 [XTrPct,XTePct,binLocs] = XToPct(Xtr(:,feaToUse),Xte(:,feaToUse), 256);
 XTrPct = uint8(XTrPct); XTePct = uint8(XTePct);
-
+%%
 rf = 0.1; J = 4; % J is number of leaf nodes in tree, rf is "random forest" parameter - # random features to choose at each iter
 boostArgs.nIter = 1000;  
 boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
@@ -17,21 +17,55 @@ funF = boostTreeVal2(boostStruct,boostArgs.nIter,XTrPct,boostArgs.v);
 
 %%
 
-%Jvals = [2 4 8 16 32 64];
-Jvals = [128 256 512 1024 2048];
-testRMSE = zeros(1,length(Jvals));
-rf = 0.1;
-boostArgs.nIter = 400;  
+Jvals = 2.^(1:15);
+testRMSEj = zeros(1,length(Jvals));
+totalRuntimes = zeros(1,length(Jvals));
+rf = 0.4;
+boostArgs.nIter = 800;  
 boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
-boostArgs.v = 0.1;     
+boostArgs.v = 0.5;     
 for jj = 1:length(Jvals)
+    t1 = clock;
     J = Jvals(jj);
     J
     boostArgs.funargs = {rf J};
     [perfTrain,perfTest,boostStruct] = ...
         BoostLS(XTrPct, Ytr, XTePct, Yte, @boostTreeFun, boostArgs, [], []);
-    testRMSE(jj) = boostStruct.perfTest(end).rmse;
+    testRMSEj(jj) = boostStruct.perfTest(end).rmse;
+    t2 = clock;
+    numSeconds = etime(t2,t1)
+    totalRuntimes(jj) = numSeconds;
 end
+
+save('tc_jValsTimeTest.mat');
+%%
+
+
+xx = Jvals(1:10);
+yy = totalRuntimes(1:10);
+coeffs = polyfit(xx,yy,1);
+aa = coeffs(1); bb = coeffs(2);
+yy2 = aa.*xx + bb;
+
+figure;
+hold on
+plot(xx,yy,'b-');
+plot(xx,yy2,'r-');
+hold off
+
+%{
+A is approximately 8 seconds
+B is abbroximately 15 minutes
+
+So it takes at least 22 minutes to do 800 iterations 
+    and each leaf node adds 7 seconds to total run time
+
+At the low end, it takes a shorter time
+
+testRMSEj was minimized at J=2^5, so we will stick with that
+
+%}
+
 %%
 J = 64;
 boostArgs.v = 0.1;
--- decisionTreeNick/runOptim.m
+++ decisionTreeNick/runOptim.m
@@ -1,6 +1,6 @@
 %{
 From testing, I found the following:
-Optimal J=128
+Optimal J=32
 v = 0.5
 rf = 0.9
 %}
@@ -9,8 +9,7 @@ feaToUse = 1:size(Xtr,2);
 [XTrPct,XTePct,binLocs] = XToPct(Xtr(:,feaToUse),Xte(:,feaToUse), 256);
 XTrPct = uint8(XTrPct); XTePct = uint8(XTePct);
 
-tic
-rf = 0.9; J = 128; % J is number of leaf nodes in tree, rf is "random forest" parameter - # random features to choose at each iter
+rf = 0.9; J = 32; % J is number of leaf nodes in tree, rf is "random forest" parameter - # random features to choose at each iter
 boostArgs.nIter = 1000;  
 boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
 boostArgs.v = 0.5;     
@@ -18,18 +17,4 @@ boostArgs.funargs = {rf J};
 
 [perfTrain,perfTest,boostStruct] = ...
     BoostLS(XTrPct, Ytr, XTePct, Yte, @boostTreeFun, boostArgs, [], []);
-J
-save('tc_J128result.mat');
-toc
-
-tic
-rf = 0.9; J = 128; % J is number of leaf nodes in tree, rf is "random forest" parameter - # random features to choose at each iter
-boostArgs.nIter = 1000;  
-boostArgs.evaliter = unique([1:10:boostArgs.nIter boostArgs.nIter]);
-boostArgs.v = 0.5;     
-boostArgs.funargs = {rf J};
-[perfTrain,perfTest,boostStruct] = ...
-    BoostLS(XTrPct, Ytr, XTePct, Yte, @boostTreeFun, boostArgs, [], []);
-J
-save('tc_J256result.mat');
-toc
+save('tc_J32result.mat');
