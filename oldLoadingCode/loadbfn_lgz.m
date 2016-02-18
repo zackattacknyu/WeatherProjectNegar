ff59cf486df2f6f53be6da80c6396e98e8691f80
zackattacknyu <zackattacknyu@gmail.com>
2016-02-18 00:45:26 -0800

    inserted old code

---
+++ oldLoadingCode/loadbfn_lgz.m
@@ -0,0 +1,44 @@
+function om = loadbfn_lgz(fn,dim,sz)
+% loadbfn -- loads a binary file fn after gunzipping to /tmp
+%	of dim ([nrows ncols]) of sz 'float','int', etc.
+%
+%	by DKB on 2/5/96
+%	  changed to big-endian by default  050930
+%	  use loadbfn_l.m  for little-end
+%
+%	revised here to gunzip to /tmp file first
+%
+
+if(nargin < 3)
+	sz = 'float32';
+end
+
+%need to fake basename to handle input
+%bn=eval(['! basename ' fn ' .gz '])
+clk = fix(clock);
+MS = sprintf('%-2.2d%-2.2d',clk(5),clk(6));
+
+lc=fliplr(find(fn == '.'));
+if(isempty(lc))
+	lc = length(fn)+1;
+end
+
+fc=fliplr(find(fn == '/'));
+if(isempty(fc))
+	fc = 0;
+end
+
+tmpn=['tmp/' fn(fc(1)+1:lc(1)-1) MS ];
+eval(['!gunzip -c ' fn ' > ' tmpn ]);
+
+%tmpn=['tmp/' fn(fc(1)+1:lc(1)-1)];
+%gunzip(fn,'tmp/');
+
+f = fopen(tmpn,'r','l');
+
+if(nargout > 0)
+	om = fread(f,flipud(dim(:))',sz)';
+end
+fclose(f);
+
+delete(tmpn);
