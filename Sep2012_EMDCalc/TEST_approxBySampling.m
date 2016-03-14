22b050b338f6462ab443bd22950e0565397e7f91
zackattacknyu <zackattacknyu@gmail.com>
2016-03-14 14:52:23 -0700

    df

---
+++ Sep2012_EMDCalc/TEST_approxBySampling.m
@@ -0,0 +1,111 @@
+load('patchesSep2011Data_results_new1.mat');
+%load('patchesSep2011Data_results_allT_new1.mat');
+
+numPatches = size(predErrorsEMD,2);
+randPatches = randperm(numPatches);
+%%
+%numRandPatches = floor(numPatches*0.6);
+emd1Array = zeros(1,numPatches);
+emd2Array = zeros(1,numPatches);
+emdOtherArray = zeros(1,numPatches);
+emdOther2Array = zeros(1,numPatches);
+varemd1 = ones(1,numPatches);
+varemd2 = ones(1,numPatches);
+varemdOther = ones(1,numPatches);
+varemd2Other = ones(1,numPatches);
+numCompare = 100;
+minFlow = min(totalFlowEMD(:));
+for ii = 1:numPatches
+    randPatchesUse = randPatches(1:ii);
+    predErrorsSampled = predErrorsEMD(:,randPatchesUse);
+    workSampled = totalWorkEMD(:,randPatchesUse);
+    flowSampled = totalFlowEMD(:,randPatchesUse);
+    res = mean(predErrorsSampled,2);
+    res2 = mean(workSampled,2)./mean(flowSampled,2);
+    %res2 = sum(predErrorsSampled.*log(flowSampled/minFlow),2)./sum(log(flowSampled/minFlow),2);
+    
+    emd1Array(ii) = res(1);
+    emdOtherArray(ii) = res2(1);
+    
+    emd2Array(ii) = res(2);
+    emdOther2Array(ii) = res2(2);
+    
+    if(ii>numCompare)
+        varemd1(ii) = var(emd1Array(ii-numCompare:ii));
+        varemd2(ii) = var(emd2Array(ii-numCompare:ii));
+        varemdOther(ii) = var(emdOtherArray(ii-numCompare:ii));
+        varemd2Other(ii) = var(emdOther2Array(ii-numCompare:ii));
+    end
+end
+
+figure
+hold on
+plot(emd1Array);
+plot(emd2Array);
+legend('Dec Tree','CCS Prediction');
+title('Average EMD Loss Function Value');
+xlabel('Number of Random Samples Averaged');
+ylabel('Average EMD');
+hold off
+
+figure
+hold on
+plot(varemd1);
+plot(varemd2);
+legend('Dec Tree','CCS Prediction');
+title('Average EMD Variance of Last 20 values');
+xlabel('Number of Random Samples Averaged');
+ylabel('Variance of Avg EMD values');
+hold off
+
+figure
+hold on
+plot(emdOtherArray);
+plot(emdOther2Array);
+legend('Dec Tree','CCS Prediction');
+title('Average EMD Loss Function Value With Weighting');
+xlabel('Number of Random Samples Averaged');
+ylabel('Average EMD Weighted');
+hold off
+
+figure
+hold on
+plot(varemdOther);
+plot(varemd2Other);
+legend('Dec Tree','CCS Prediction');
+title('Average Weighted EMD Variance of Last 20 values');
+xlabel('Number of Random Samples Averaged');
+ylabel('Variance of Avg Weighted EMD values');
+hold off
+
+%%
+
+
+load('patchesSep2011Data_new1.mat');
+%%
+
+patchesSum = zeros(1,length(targetPatches));
+for ii = 1:length(targetPatches)
+    curPatch = targetPatches{ii};
+    patchesSum(ii) = sum(curPatch(:));
+end
+
+%{
+
+NO CORRELATION BETWEEN 
+    SINGLE PATCH CHARACTERISTICS
+    AND EMD FOUND
+
+patchesDiff = zeros(1,length(targetPatches));
+patchesEMD = predErrorsEMD(1,:);
+for jj = 1:length(targetPatches)
+    curPatch = targetPatches{jj};
+    curPatchPred = predPatches{1,jj};
+    %patchesDiff(jj) = sum((curPatch(:)-curPatchPred(:)).^2);
+    %patchesDiff(jj) = sum(abs(curPatch(:)-curPatchPred(:)));
+    patchesDiff(jj) = abs(sum(curPatch(:))-sum(curPatchPred(:)));
+end
+
+plot(patchesDiff,patchesEMD,'r.');
+
+%}
--- Sep2012_EMDCalc/TEST_computePatchErrors_Sep2011.m
+++ Sep2012_EMDCalc/TEST_computePatchErrors_Sep2011.m
@@ -1,4 +1,4 @@
-
+%{
 t1 = clock;
 CURRENT_generatePatchErrors_multiThreaded( 'patchesSep2011Data_all2.mat',...
     'patchesSep2011Data_results_all2.mat' )
@@ -20,3 +20,48 @@ t2 = clock;
 numSeconds = etime(t2,t1)
 save('patchesSep2011Data_numSeconds_all4.mat','numSeconds');
 
+
+
+t1 = clock;
+CURRENT_generatePatchErrors_multiThreaded( 'patchesSep2011Data_new1.mat',...
+    'patchesSep2011Data_results_new1.mat' )
+t2 = clock;
+numSeconds = etime(t2,t1)
+save('patchesSep2011Data_numSeconds_new1.mat','numSeconds');
+
+t1 = clock;
+CURRENT_generatePatchErrors_multiThreaded( 'patchesSep2011Data_new2.mat',...
+    'patchesSep2011Data_results_new2.mat' )
+t2 = clock;
+numSeconds = etime(t2,t1)
+save('patchesSep2011Data_numSeconds_new2.mat','numSeconds');
+
+t1 = clock;
+CURRENT_generatePatchErrors_multiThreaded( 'patchesSep2011Data_new3.mat',...
+    'patchesSep2011Data_results_new3.mat' )
+t2 = clock;
+numSeconds = etime(t2,t1)
+save('patchesSep2011Data_numSeconds_new3.mat','numSeconds');
+
+%}
+
+t1 = clock;
+CURRENT_generatePatchErrors_multiThreaded( 'patchesSep2011Data_allT_new1.mat',...
+    'patchesSep2011Data_results_allT_new1.mat' )
+t2 = clock;
+numSeconds = etime(t2,t1)
+save('patchesSep2011Data_numSeconds_allT_new1.mat','numSeconds');
+
+t1 = clock;
+CURRENT_generatePatchErrors_multiThreaded( 'patchesSep2011Data_allT_new2.mat',...
+    'patchesSep2011Data_results_allT_new2.mat' )
+t2 = clock;
+numSeconds = etime(t2,t1)
+save('patchesSep2011Data_numSeconds_allT_new2.mat','numSeconds');
+
+t1 = clock;
+CURRENT_generatePatchErrors_multiThreaded( 'patchesSep2011Data_allT_new3.mat',...
+    'patchesSep2011Data_results_allT_new3.mat' )
+t2 = clock;
+numSeconds = etime(t2,t1)
+save('patchesSep2011Data_numSeconds_allT_new3.mat','numSeconds');
